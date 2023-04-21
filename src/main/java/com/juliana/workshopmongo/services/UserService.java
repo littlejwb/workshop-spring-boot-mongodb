682d2f589ba21c2c4f8f9d59640224d5e2c9f2a8
Juliana Ricardo <szric.ju@gmail.com>
2023-04-20 21:05:12 -0300

    Insert user with POST

--- src/main/java/com/juliana/workshopmongo/services/UserService.java
+++ src/main/java/com/juliana/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.juliana.workshopmongo.services;
 
 import com.juliana.workshopmongo.domain.User;
+import com.juliana.workshopmongo.dto.UserDTO;
 import com.juliana.workshopmongo.repository.UserRepository;
 import com.juliana.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
